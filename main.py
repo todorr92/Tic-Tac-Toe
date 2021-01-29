f85da5d2a969d75fdf7676bbf2572caefd654b80
unknown <todorovicslobodan10@gmail.com>
2021-01-29 15:22:11 +0000

    Implement function to draw x's or o's

--- main.py
+++ main.py
@@ -9,9 +9,16 @@ HEIGHT = 600
 LINE_WIDTH = 15
 BOARD_ROWS = 3
 BOARD_COLS = 3
+SQUARE_SIZE = 200
+CIRCLE_RADIUS = 60
+CIRCLE_WIDTH = 15
 RED = (255, 0, 0)
 BG_COLOR = (28, 170, 156)
 LINE_COLOR = (23, 145, 135)
+CIRCLE_COLOR = (239, 231, 200)
+CROSS_WIDTH = 25
+SPACE = 50
+CROSS_COLOR = (66, 66, 66)
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tic-Tac-Toe")
 window.fill(BG_COLOR)
@@ -30,6 +37,19 @@ def draw_lines():
     pygame.draw.line(window, LINE_COLOR, (200, 0), (200, 600), LINE_WIDTH)
     # second vertical line
     pygame.draw.line(window, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
+
+
+def draw_figures():
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == 1:
+                pygame.draw.circle(window, CIRCLE_COLOR, (int(
+                    col * 200 + 100), int(row * 200 + 100)), CIRCLE_RADIUS, CIRCLE_WIDTH)
+            elif board[row][col] == 2:
+                pygame.draw.line(window, CROSS_COLOR, (col * SQUARE_SIZE + SPACE, row * SQUARE_SIZE + SQUARE_SIZE -
+                                                       SPACE), (col * SQUARE_SIZE + SQUARE_SIZE - SPACE, row * SQUARE_SIZE + SPACE), CROSS_WIDTH)
+                pygame.draw.line(window, CROSS_COLOR, (col * SQUARE_SIZE + SPACE, row * SQUARE_SIZE + SPACE),
+                                 (col * SQUARE_SIZE + SQUARE_SIZE - SPACE, row * SQUARE_SIZE + SQUARE_SIZE - SPACE), CROSS_WIDTH)
 
 
 def mark_square(row, col, player):
@@ -53,9 +73,26 @@ def is_board_full():
 
 draw_lines()
 
+player = 1
 # main loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x = event.pos[0]
+            mouse_y = event.pos[1]
+
+            clicked_row = int(mouse_y // 200)
+            clicked_col = int(mouse_x // 200)
+
+            if available_square(clicked_row, clicked_col):
+                if player == 1:
+                    mark_square(clicked_row, clicked_col, 1)
+                    player = 2
+                elif player == 2:
+                    mark_square(clicked_row, clicked_col, 2)
+                    player = 1
+                draw_figures()
+
     pygame.display.update()
