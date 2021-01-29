758dcbae90b2d838c352a411f0eb4671d6105342
unknown <todorovicslobodan10@gmail.com>
2021-01-29 09:33:38 +0000

    Initial commit, set up window and game title

--- main.py
+++ main.py
@@ -6,4 +6,11 @@ pygame.init()
 WIDTH = 600
 HEIGHT = 600
 
-window = pygame.display.set_mode(WIDTH, HEIGHT)
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tic-Tac-Toe")
+
+# main loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
